af4247def74d4e1f034d00732c0b5e921056a45b
Beyang Liu <beyang@sourcegraph.com>
2014-08-21 14:19:07 -0700

    resolve variable to type when looking up scope

--- jedi/evaluate/dynamic.py
+++ jedi/evaluate/dynamic.py
@@ -95,7 +95,7 @@ def search_params(evaluator, param):
                         continue
                     scopes = [scope]
                     if first:
-                        scopes = evaluator.eval_call_path(iter(first), c.parent, pos)
+                        scopes = evaluator.eval_call_path(iter(first), c.parent, pos, force_resolve_vars_to_types=True)
                         pos = None
                     from jedi.evaluate import representation as er
                     for scope in scopes:
